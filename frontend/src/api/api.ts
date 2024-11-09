a838b4234267dbb2762ff39cb1d631bab4c9cf7c
SlaveRRR <slava.arhipov40020141@gmail.com>
2024-11-09 18:51:32 +0300

    refresh token fix

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,15 +1,15 @@
 import { AccesTokenResponse, SignInParams, SignUpParams } from '@/types';
 import axios from 'axios';
 
-// const apiUrl = 'http://localhost:8000/api/';
-const apiUrl = import.meta.env.VITE_API 
+const apiUrl = 'http://localhost:8000/api/';
+// const apiUrl = import.meta.env.VITE_API 
 export const api = axios.create({
   baseURL: apiUrl,
   withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
-  const token = JSON.parse(localStorage.getItem('token') || "")
+  const token = JSON.parse(localStorage.getItem('token'))
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -37,11 +37,15 @@ api.interceptors.response.use(
 
 class AuthApi {
   async signIn(data: SignInParams) {
-    return axios.post<AccesTokenResponse>(`${apiUrl}signin/`, data);
+    return axios.post<AccesTokenResponse>(`${apiUrl}signin/`, data,{
+      withCredentials:true
+    });
   }
 
   async signUp(data: SignUpParams) {
-    return axios.post<AccesTokenResponse>(`${apiUrl}signup/`, data);
+    return axios.post<AccesTokenResponse>(`${apiUrl}signup/`, data,{
+      withCredentials:true
+    });
   }
 
   async refreshToken() {
