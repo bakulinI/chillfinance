772fd83345c9484786059e0ba7096b9484eb1529
SlaveRRR <slava.arhipov40020141@gmail.com>
2024-11-09 23:14:04 +0300

    fix env

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define:{
+    "process.env.VITE_API":JSON.stringify(process.env.VITE_API),
+  },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
