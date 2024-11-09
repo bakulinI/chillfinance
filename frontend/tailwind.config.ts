e997bc56d1705f61f15f573ebbd47d83f58a393a
SlaveRRR <slava.arhipov40020141@gmail.com>
2024-11-09 23:02:42 +0300

    public images fix

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
 
     backgroundImage: {
       main: 'linear-gradient(162.46deg, #3D8CEC 0%, #3DB66B 100%);',
-      auth: "url('./bg.svg')",
+      auth: "url('/bg.svg')",
     },
     borderColor: {
       input: '#D9D9D9',
