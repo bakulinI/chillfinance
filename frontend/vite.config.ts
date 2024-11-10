dc52722296b7060f4ecd9e6a0f3b5ddbd7c73d09
SlaveRRR <slava.arhipov40020141@gmail.com>
2024-11-10 20:32:25 +0300

    settings pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,66 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      workbox: {
+        globPatterns: ['**/*'],
+        maximumFileSizeToCacheInBytes: 2621440,
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.href.includes('pythonanywhere'),
+            handler: 'CacheFirst' as const,
+            options: {
+              cacheName: 'cache-backend',
+              cacheableResponse: {
+                statuses: [200],
+              },
+            },
+          },
+        ],
+      },
+      includeAssets: ['/logo_192.svg', '/logo_512.png', '/logo_180.png', '/Logo.svg'],
+      manifest: {
+        name: 'Chillfinance',
+        short_name: 'Chillfinance',
+        description: 'Chillfinance app',
+        icons: [
+          {
+            src: '/logo_192.svg',
+            sizes: '192x192',
+            type: 'image/svg',
+            purpose: 'favicon',
+          },
+          {
+            src: '/logo_512.png',
+            sizes: '512x512',
+            type: 'image/svg',
+            purpose: 'favicon',
+          },
+          {
+            src: '/logo_180.png',
+            sizes: '180x180',
+            type: 'image/svg',
+            purpose: 'apple touch icon',
+          },
+          {
+            src: '/Logo.svg',
+            sizes: '512x512',
+            type: 'image/svg',
+            purpose: 'any maskable',
+          },
+        ],
+        theme_color: '#181818',
+        background_color: '#e0cc3b',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        orientation: 'portrait',
+      },
+    }),
+  ],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
